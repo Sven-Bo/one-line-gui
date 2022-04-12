d47167d0d4f732be7fdaa1de110d0611023133dd
Sven-Bo <jubbel3@gmx.net>
2022-04-12 18:00:52 +0200

    Update ReadMe

--- README.md
+++ README.md
@@ -5,6 +5,21 @@ In this Python tutorial, I'm going to show you how to make a GUI with just one l
 ## Video Tutorial
 [![YouTube Video](https://img.youtube.com/vi/_H5hsUwv8lE/0.jpg)](https://youtu.be/_H5hsUwv8lE)
 
+## Examples
+| Code | Widow |
+| -- | -- |
+| ``` sg.popup_get_folder("Select Folder") ``` |  ![Kiku](demos/popup_get_folder.jpg) |
+| ``` sg.popup_get_file("Choose Excel file", multiple_files=True, file_types=(("Excel Files", "*.xls*"),),) ``` |  ![Kiku](demos/popup_get_file.jpg) |
+| ``` sg.popup_get_date() ``` |  ![Kiku](demos/popup_get_date.jpg) |
+| ``` sg.popup_get_text("Please enter a text:") ``` |  ![Kiku](demos/popup_get_text.jpg) |
+| ``` sg.popup_no_buttons("You cannot click any buttons") ``` |  ![Kiku](demos/popup_no_buttons.jpg) |
+| ``` sg.popup_no_titlebar("A very simple popup") ``` |  ![Kiku](demos/popup_no_titlebar.jpg) |
+| ``` sg.popup_ok("You can only click on 'OK'") ``` |  ![Kiku](demos/popup_ok.jpg) |
+| ``` sg.popup_error("Something went wrong") ``` |  ![Kiku](demos/popup_error.jpg) |
+| ``` sg.popup_notify("Task done!") ``` |  ![Kiku](demos/popup_notify.jpg) |
+| ``` sg.popup_yes_no("Do you like this video?") ``` |  ![Kiku](demos/popup_yes_no.jpg) |
+| ``` sg.Window("Continue?",[[sg.T("Do you want to subscribe to this channel?")], [sg.Yes(s=10), sg.No(s=10), sg.Button('Maybe', s=10)]],disable_close=True,).read(close=True) ``` |  ![Kiku](demos/custom.jpg) |
+
 
 ## Requirements
 ```
--- one_line_gui.py
+++ one_line_gui.py
@@ -4,7 +4,7 @@ import PySimpleGUI as sg  # pip install pysimplegui
 # https://pysimplegui.readthedocs.io/en/latest/call%20reference/#popups-pep8-versions
 
 
-### -- Display popup with text entry field and browse button so that a folder can be chosen.
+## -- Display popup with text entry field and browse button so that a folder can be chosen.
 # dir_path = sg.popup_get_folder("Select Folder")
 # if not dir_path:
 #     sg.popup("Cancel", "No folder selected")
@@ -14,7 +14,7 @@ import PySimpleGUI as sg  # pip install pysimplegui
 
 
 
-### -- Display popup window with text entry field and browse button so that a file can be chosen by user.
+## -- Display popup window with text entry field and browse button so that a file can be chosen by user.
 # file_types=Tuple[Tuple[str,str]]
 # fname = sg.popup_get_file("Choose Excel file", multiple_files=True, file_types=(("Excel Files", "*.xls*"),),)
 # if not fname:
@@ -25,7 +25,7 @@ import PySimpleGUI as sg  # pip install pysimplegui
 
 
 
-# ### -- Display a calendar window, get the user's choice, return as a tuple (mon, day, year)
+### -- Display a calendar window, get the user's choice, return as a tuple (mon, day, year)
 # date = sg.popup_get_date()
 # if not date:
 #     sg.popup("Cancel", "No date picked")
@@ -34,7 +34,7 @@ import PySimpleGUI as sg  # pip install pysimplegui
 #     sg.popup("The date you chose was", date)
 
 
-# ### -- Display Popup with text entry field. Returns the text entered or None if closed / cancelled
+### -- Display Popup with text entry field. Returns the text entered or None if closed / cancelled
 # text = sg.popup_get_text("Please enter a text:")
 # if not text:
 #     sg.popup("Cancel", "No text was entered")
@@ -43,41 +43,41 @@ import PySimpleGUI as sg  # pip install pysimplegui
 #     sg.popup("You have entered", text)
 
 
-# ### -- Show a Popup but without any buttons
+### -- Show a Popup but without any buttons
 # sg.popup_no_buttons("You cannot click any buttons")
-#
-#
-# ### -- Display a Popup without a titlebar. Enables grab anywhere so you can move it
+
+
+### -- Display a Popup without a titlebar. Enables grab anywhere so you can move it
 # sg.popup_no_titlebar("A very simple popup")
-#
-#
-#
-### -- Display Popup with OK button only
+
+
+
+## -- Display Popup with OK button only
 # sg.popup_ok("You can only click on 'OK'")
-#
-#
-#
-# ### -- Popup with colored button and 'Error' as button text
+
+
+
+### -- Popup with colored button and 'Error' as button text
 # sg.popup_error("Something went wrong")
-#
-#
-#
-# ### -- Displays a "notification window", usually in the bottom right corner of your display.
-# ###    The window will slowly fade in and out if desired.
+
+
+
+### -- Displays a "notification window", usually in the bottom right corner of your display.
+###    The window will slowly fade in and out if desired.
 # sg.popup_notify("Task done!")
-#
-#
-#
-# ### -- Display Popup with Yes and No buttons
+
+
+
+### -- Display Popup with Yes and No buttons
 # answer = sg.popup_yes_no("Do you like this video?")
 # sg.popup("You have selected", answer)
-#
-#
-#
+
+
+
 # ### -- You can easily make your own popup. Popups can be accomplished in 1 line of code:
-choice, _ = sg.Window(
-    "Continue?",
-    [[sg.T("Do you want to subscribe to this channel?")], [sg.Yes(s=10), sg.No(s=10), sg.Button('Maybe', s=10)]],
-    disable_close=True,
-).read(close=True)
-sg.popup("Your choice was", choice)
+# choice, _ = sg.Window(
+#     "Continue?",
+#     [[sg.T("Do you want to subscribe to this channel?")], [sg.Yes(s=10), sg.No(s=10), sg.Button('Maybe', s=10)]],
+#     disable_close=True,
+# ).read(close=True)
+# sg.popup("Your choice was", choice)
